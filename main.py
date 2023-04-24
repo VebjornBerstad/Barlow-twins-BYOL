3df00682045dd6b2a87249e0187b32848f4ed986
VebjornBerstad <91483949+VebjornBerstad@users.noreply.github.com>
2023-04-25 00:40:06 +0200

    Modifcation

--- main.py
+++ main.py
@@ -1,80 +1,56 @@
-from models import barlowBYOL, mlp, SmallConvNet, LinearEvalModel
+from models import barlowBYOL, LinearEvaluationCallback
 
 import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 import torch.nn as nn
-from torch.nn import Flatten
 
 from torchvision.models import resnet18
 
 from pytorch_lightning.loggers import TensorBoardLogger
+import matplotlib.pyplot as plt
 
 
 def main():
 
-    
-    data_transforms = {
-        'train': transforms.Compose([
-            transforms.RandomResizedCrop(32),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-        ]),
-        'val': transforms.Compose([
-            transforms.Resize(32),
-            transforms.CenterCrop(32),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-        ]),
-    }
-
-    transform = transforms.Compose(
-        [transforms.Resize((224, 224)), transforms.ToTensor()]
-    )
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        ])
 
     # Load the CIFAR-10 dataset
     train_dataset = CIFAR10(root='./data', train=True, download=True, transform=transform)
     val_dataset = CIFAR10(root='./data', train=False, download=True, transform=transform)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4)
-    # val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=4)
+    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, pin_memory=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
 
     # Load the pre-trained SmallConvNet encoder
-    # pretrained_encoder = SmallConvNet(512)
 
-    def resnet18_encoder(pretrained=True):
-        resnet = resnet18(pretrained=pretrained)
-        encoder = nn.Sequential(
-            *list(resnet.children())[:-2],  # Remove the avgpool and fc layers
-            nn.AdaptiveAvgPool2d((1, 1)),   # Add adaptive average pooling to reduce the spatial dimensions to 1x1
-            Flatten(),                      # Add a Flatten layer to convert the output tensor to a 1D tensor
-        )
-        return encoder
+    encoder = resnet18()
+    encoder.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    encoder.maxpool = nn.MaxPool2d(kernel_size=1, stride=1)
+    encoder.fc = nn.Identity()
 
     # Load the barlowBYOL model and train it
-    barlow_byol = barlowBYOL(encoder=resnet18_encoder(), projector=mlp(512), image_size=(32, 32), lr=3e-4, tau=0.99)
-    barlow_byol_trainer = Trainer(accelerator="gpu", max_epochs=10)
-    barlow_byol_trainer.fit(barlow_byol, train_dataloader)
 
-    # Remove the projector from the pre-trained encoder
-    pretrained_encoder = barlow_byol.online[0]
+    logger = TensorBoardLogger("logs", name="Barlow_BYOL")
+    
+    barlow_byol = barlowBYOL(encoder=encoder, image_size=(32, 32), lr=3e-4, tau=0.99, encoder_out_dim=512)
 
-    train_dataset = CIFAR10(root='./data', train=True, download=True, transform=data_transforms['train'])
-    val_dataset = CIFAR10(root='./data', train=False, download=True, transform=data_transforms['val'])
+    linear_evaluation = LinearEvaluationCallback(encoder_output_dim=512, num_classes=10)
+    checkpoint_callback = ModelCheckpoint(every_n_epochs=100, save_top_k=-1, save_last=True)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4)
-    val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=4)
-
-    # Perform linear evaluation
-    tb_logger = TensorBoardLogger("logs/")
-
-    linear_eval_model = LinearEvalModel(encoder=pretrained_encoder, num_classes=10, lr=3e-4)
-    linear_eval_trainer = Trainer(logger=tb_logger, accelerator="gpu", max_epochs=10)
-    linear_eval_trainer.fit(linear_eval_model, train_dataloader, val_dataloader)
+    barlow_byol_trainer = Trainer(
+        devices=1,
+        accelerator='gpu',
+        max_epochs=500,
+        callbacks=[linear_evaluation, checkpoint_callback],
+        logger=logger,
+        )
+    barlow_byol_trainer.fit(barlow_byol, train_dataloader, val_dataloader)
 
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -1,22 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import pytorch_lightning as pl
-import torchvision.transforms as T
+from torchmetrics.functional import accuracy
 
+import pytorch_lightning as pl
 
 from copy import deepcopy
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Sequence, Union
 
 from kornia import augmentation as aug
-from kornia import filters
-from kornia.geometry import transform as tf
 
 import random
-
-def set_requires_grad(model: nn.Module, requires_grad: bool):
-    for param in model.parameters():
-        param.requires_grad = requires_grad
 
 class RandomApply(nn.Module):
     def __init__(self, fn: Callable, p: float):
@@ -33,47 +27,54 @@ def default_augmentation(image_size: Tuple[int, int] = (32, 32)) -> nn.Module:
         aug.RandomCrop(size=image_size, padding=4, padding_mode='reflect'),
         aug.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
         aug.RandomGrayscale(p=0.1),
-        aug.Normalize(
-            mean=torch.tensor([0.4914, 0.4822, 0.4465]),
-            std=torch.tensor([0.2470, 0.2435, 0.2616]),
-        ),
     )
 
-def mlp(dim: int, projection_size: int = 256, hidden_size: int = 4096) -> nn.Module:
-    return nn.Sequential(
-        nn.Linear(dim, hidden_size),
-        nn.BatchNorm1d(hidden_size),
-        nn.ReLU(inplace=True),
-        nn.Linear(hidden_size, projection_size),
-    )
+class BarlowTwinsLoss(nn.Module):
+    def __init__(self, batch_size, lambda_coeff=5e-3, z_dim=128):
+        super().__init__()
 
-class SmallConvNet(nn.Module):
-    def __init__(self, output_dim):
-        super(SmallConvNet, self).__init__()
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+        self.z_dim = z_dim
+        self.batch_size = batch_size
+        self.lambda_coeff = lambda_coeff
+
+    def off_diagonal_ele(self, x):
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+    def forward(self, z1, z2):
+        # N x D, where N is the batch size and D is output dim of projection head
+        z1_norm = (z1 - torch.mean(z1, dim=0)) / torch.std(z1, dim=0)
+        z2_norm = (z2 - torch.mean(z2, dim=0)) / torch.std(z2, dim=0)
+
+        cross_corr = torch.matmul(z1_norm.T, z2_norm) / self.batch_size
+
+        on_diag = torch.diagonal(cross_corr).add_(-1).pow_(2).sum()
+        off_diag = self.off_diagonal_ele(cross_corr).pow_(2).sum()
+
+        return on_diag + self.lambda_coeff * off_diag
+
+class ProjectionHead(nn.Module):
+    def __init__(self, input_dim=2048, hidden_dim=2048, output_dim=128):
+        super().__init__()
+
+        self.projection_head = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim, bias=True),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim, bias=False),
         )
-        self.fc = nn.Linear(32 * 8 * 8, output_dim)
 
     def forward(self, x):
-        x = self.conv_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
-
+        return self.projection_head(x)
 
 class barlowBYOL(pl.LightningModule):
     def __init__(self, 
                  encoder, 
-                 projector, 
+                 encoder_out_dim, 
                  image_size: Tuple[int, int], 
                  lr=3e-4, 
-                 tau=0.99
+                 tau=0.99,
                  ):
         
         super().__init__()
@@ -82,58 +83,36 @@ class barlowBYOL(pl.LightningModule):
         self.lr = lr
         self.tau = tau
 
-        self.online = nn.Sequential(encoder, projector)
+        self.encoder = encoder
+        self.projection_head = ProjectionHead(input_dim=encoder_out_dim)
         self._target = None
 
+        self.loss = BarlowTwinsLoss(batch_size=256)
+
         # Create and initialize the projector with a dummy tensor
-        self.online(torch.zeros(2,3, *image_size))
+        # self.online(torch.zeros(2,3, *image_size))
 
     @property
     def target(self):
         if self._target is None:
-            self._target = deepcopy(self.online)
-        return self._target
+            target_encoder = deepcopy(self.encoder)
+            target_projection_head = deepcopy(self.projection_head)
+        return nn.Sequential(target_encoder, target_projection_head)
     
     def forward(self, x):
-        x = self.online[0](x)  # Pass input through the ResNet-18 encoder
-        x = x.view(x.size(0), -1)  # Flatten the output tensor
-        x = self.online[1](x)  # Pass the flattened tensor through the projector
-        return x
-
-    
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
-    
-    def cross_corr_loss(self, y1, y2, target_y1, target_y2):
-        batch_size = y1.size(0)
-        y1_norm = F.normalize(y1, dim=1)
-        y2_norm = F.normalize(y2, dim=1)
-        target_y1_norm = F.normalize(target_y1, dim=1)
-        target_y2_norm = F.normalize(target_y2, dim=1)
-
-        c = torch.matmul(y1_norm, y2_norm.T) / batch_size
-        c_target = torch.matmul(target_y1_norm, target_y2_norm.T) / batch_size
-        c_diff = c - c_target.detach()
-        loss = torch.sum(c_diff ** 2) / (batch_size ** 2)
-
-        return loss
+        return self.encoder(x)
     
     def training_step(self, batch, batch_idx):
         x = batch[0]
 
         with torch.no_grad():
             x1, x2 = self.augment(x), self.augment(x)
+            target_y = self.projection_head(self.encoder(x2))
+        y = self.projection_head(self.encoder(x1))
 
-        y1, y2 = self.forward(x1), self.forward(x2)
-
-        with torch.no_grad():
-            target_y1, target_y2 = self.target(x1), self.target(x2)
-
-        loss = self.cross_corr_loss(y1, y2, target_y1, target_y2)
-        
-        self.log('train_loss', loss)
-
+        loss = self.loss(y, target_y)
+        self.log("train_loss", loss, on_step=True, on_epoch=False)
+        print(f'\nTrain loss: {loss}')
         return loss
     
     def validation_step(self, batch, batch_idx):
@@ -141,50 +120,90 @@ class barlowBYOL(pl.LightningModule):
 
         with torch.no_grad():
             x1, x2 = self.augment(x), self.augment(x)
+            target_y = self.projection_head(self.encoder(x2))
+        y = self.projection_head(self.encoder(x1))
 
-        y1, y2 = self.forward(x1), self.forward(x2)
+        loss = self.loss(y, target_y)
 
-        with torch.no_grad():
-            target_y1, target_y2 = self.target(x1), self.target(x2)
-
-        loss = self.cross_corr_loss(y1, y2, target_y1, target_y2)
-
-        self.log('val_loss', loss)
-
-        return loss
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
     
     def on_after_backward(self):
-        for online_param, target_param in zip(self.online.parameters(), self.target.parameters()):
+        for online_param, target_param in zip(self.encoder.parameters(), self.target[0].parameters()):
+            target_param.data = target_param.data * self.tau + online_param.data * (1 - self.tau)
+        for online_param, target_param in zip(self.projection_head.parameters(), self.target[1].parameters()):
             target_param.data = target_param.data * self.tau + online_param.data * (1 - self.tau)
 
-class LinearEvalModel(pl.LightningModule):
-    def __init__(self, encoder, num_classes, lr=3e-4):
+class LinearEvaluationCallback(pl.Callback):
+    def __init__(
+            self,
+            encoder_output_dim: int,
+            num_classes: int
+            ):
         super().__init__()
-        self.encoder = encoder
-        self.classifier = nn.Linear(512, num_classes)
-        self.lr = lr
-        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer: torch.optim.Optimizer
 
-        set_requires_grad(self.encoder, False)
+        self.encoder_output_dim = encoder_output_dim
+        self.num_classes = num_classes
 
-    def forward(self, x):
-        features = self.encoder(x)
-        return self.classifier(features)
+    def on_fit_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
+        pl_module.linear_classifier = nn.Linear(self.encoder_output_dim, self.num_classes).to(pl_module.device)
+        self.optimizer = torch.optim.Adam(pl_module.linear_classifier.parameters(), lr=1e-4)
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
-
-    def training_step(self, batch, batch_idx):
+    def extract_batch(self, batch: Sequence, device: Union[str, torch.device]):
         x, y = batch
-        logits = self.forward(x)
-        loss = self.criterion(logits, y)
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+        x = x.to(device)
+        y = y.to(device)
 
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self.forward(x)
-        loss = self.criterion(logits, y)
-        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+        return x, y
+    
+    def on_train_batch_end(
+            self,
+            trainer: pl.Trainer,
+            pl_module: pl.LightningModule,
+            outputs: Sequence,
+            batch: Sequence,
+            batch_idx: int
+    ):
+        x, y = self.extract_batch(batch, pl_module.device)
+
+        with torch.no_grad():
+            features = pl_module.forward(x)
+        
+        features = features.detach()
+        preds = pl_module.linear_classifier(features)
+        loss = F.cross_entropy(preds, y)
+
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        pred_labels = torch.argmax(preds, dim=1)
+        acc = accuracy(pred_labels, y, task="multiclass", num_classes=10)
+        pl_module.log("online_train_acc", acc, on_step=True, on_epoch=False)
+        pl_module.log("online_train_loss", loss, on_step=True, on_epoch=False)
+
+    def on_validation_batch_end(
+            self,
+            trainer: pl.Trainer,
+            pl_module: pl.LightningModule,
+            outputs: Sequence,
+            batch: Sequence,
+            batch_idx: int
+    ):
+        x, y = self.extract_batch(batch, pl_module.device)
+
+        with torch.no_grad():
+            features = pl_module.forward(x)
+        
+        features = features.detach()
+        preds = pl_module.linear_classifier(features)
+        loss = F.cross_entropy(preds, y)
+        
+        pred_labels = torch.argmax(preds, dim=1)
+        acc = accuracy(pred_labels, y, task="multiclass", num_classes=10)
+        pl_module.log("online_val_acc", acc, on_step=False, on_epoch=True, sync_dist=True)
+        pl_module.log("online_val_loss", loss, on_step=False, on_epoch=True, sync_dist=True)
